3cd2e04b1903e50edea020937d2390a052f0d09b
Bohdan Tkachenko <bohdan@tkachenko.dev>
2025-09-28 23:30:24 -0400

    Assign play/prev/next track controls to buttons

--- dot_config/home-manager/modules/gnome.nix
+++ dot_config/home-manager/modules/gnome.nix
@@ -1,11 +1,17 @@
 {
   dconf.settings = {
     "org/gnome/desktop/wm/keybindings" = {
-      close = [ "<Control>q" ];
+      close = [ "<Shift><Control>q" ];
       switch-applications = [ "<Control>Tab" ];
       switch-applications-backward = [ "<Shift><Control>Tab" ];
       switch-group = [ "<Control>grave" ];
       switch-group-backward = [ "<Shift><Control>grave" ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      next = [ "AudioNext" ];
+      play = [ "AudioPlay" ];
+      previous = [ "AudioPrev" ];
     };
   };
 }
