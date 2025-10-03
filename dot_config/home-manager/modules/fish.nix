cd006b9a699cbbaaa0fa2d15e89cb692dad379b7
Bohdan Tkachenko <bohdan@tkachenko.dev>
2025-10-03 00:10:17 -0400

    fish config

--- dot_config/home-manager/modules/fish.nix
+++ dot_config/home-manager/modules/fish.nix
@@ -1,7 +1,82 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.fish = {
     enable = true;
+
+    shellAliases = {
+      l = "eza --icons --group-directories-first -lah";
+      t = "eza --icons --group-directories-first -L 2 -Tlah";
+    };
+
+    shellAbbrs = {
+      cm = "chezmoi";
+
+      m = "micro";
+      vi = "micro";
+      vim = "micro";
+      nano = "micro";
+      ed = "micro";
+      editor = "micro";
+
+      tt = "eza --icons --group-directories-first -L 3 -Tlah";
+
+      rm = "trash";
+      cat = "bat";
+
+      grep = "ug";
+      egrep = "ug -E";
+      fgrep = "ug -F";
+      xzgrep = "ug -z";
+      xzegrep = "ug -zE";
+      xzfgrep = "ug -zF";
+
+      gst = "git status";
+      gd = "git diff";
+      gp = "git push";
+      gl = "git pull";
+      gx = "git log";
+      gc = "git commit";
+      ga = "git add";
+      gaa = "git add -A";
+      grm = "git rm";
+      gmv = "git mv";
+      gcp = "git cp";
+      gco = "git checkout";
+      gb = "git branch";
+
+      k = "kubectl";
+      kube = "kubectl";
+
+      tf = "tofu";
+      terraform = "tofu";
+      tg = "terragrunt";
+    };
+
+    plugins = with pkgs.fishPlugins; [
+      {
+        name = "tide";
+        inherit (tide) src;
+      }
+
+      {
+        name = "sponge";
+        inherit (z) src;
+      }
+
+      {
+        name = "z";
+        inherit (z) src;
+      }
+    ];
+
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+      set fish_color_command green --bold
+    '';
   };
+
+  home.activation.configure-tide = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ${pkgs.fish}/bin/fish -c "tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Many icons' --transient=Yes"
+  '';
 }
