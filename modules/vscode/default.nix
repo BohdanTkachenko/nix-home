9358c098d20b955f75357467483222b1cfc653f1
Bohdan Tkachenko <bohdan@tkachenko.dev>
2025-10-26 18:05:50 -0400

    fix(vscode): properly merge extensions

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -15,17 +15,16 @@
     vscode = {
       enable = true;
       package = (config.lib.nixGL.wrap pkgs.vscode);
-      profiles.default = {
-        extensions = with pkgs.vscode-extensions; [
-          foxundermoon.shell-format
-          hashicorp.terraform
-          jnoortheen.nix-ide
-          ms-python.python
-          ms-vscode.makefile-tools
-        ];
-      };
     };
   };
+
+  programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
+    foxundermoon.shell-format
+    hashicorp.terraform
+    jnoortheen.nix-ide
+    ms-python.python
+    ms-vscode.makefile-tools
+  ];
 
   home.file.".config/Code/User/settings.json".source = lib.mkForce (
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/modules/vscode/settings.json"
--- modules/vscode/personal.nix
+++ modules/vscode/personal.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 {
-  programs.vscode.profiles.default = {
-    extensions = with pkgs.vscode-extensions; [
-      hashicorp.terraform
-      ms-vscode.makefile-tools
-    ];
-  };
+  programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
+    hashicorp.terraform
+    ms-vscode.makefile-tools
+  ];
 
   imports = [
     ./default.nix
