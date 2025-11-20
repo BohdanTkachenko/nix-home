75560393646a18391753a5541225c86e396e63b0
Bohdan Tkachenko <bohdan@tkachenko.dev>
2025-11-20 00:18:15 -0500

    feat(jj): some basic aliases

--- modules/jujutsu/default.nix
+++ modules/jujutsu/default.nix
@@ -7,6 +7,38 @@
         email = "bohdan@tkachenko.dev";
         name = "Bohdan Tkachenko";
       };
+
+      aliases = {
+        s = [ "status" ];
+        x = [
+          "log"
+          "-r"
+          "(main..@):: | (main..@)-"
+        ];
+        f = [
+          "git"
+          "fetch"
+        ];
+        y = [
+          "util"
+          "exec"
+          "--"
+          "sh"
+          "-c"
+          "jj git fetch && jj rebase -d main@origin"
+        ];
+        p = [
+          "git"
+          "push"
+        ];
+        main = [
+          "bookmark"
+          "set"
+          "main"
+          "-r"
+          "@"
+        ];
+      };
     };
   };
 }
