7069d66895cd3cd31f36663b1ff1b4f3d53f0f07
Bohdan Tkachenko <bohdan@tkachenko.dev>
2025-11-27 17:08:09 -0500

    feat(steam): fix OW2 icon in dock

--- machines/personal-pc/default.nix
+++ machines/personal-pc/default.nix
@@ -43,4 +43,15 @@
       exec = "steam -silent";
     })
   ];
+
+  home-manager.users.dan.xdg.desktopEntries.overwatch2 = {
+    name = "Overwatch 2";
+    comment = "Play this game on Steam";
+    exec = "steam steam://rungameid/2357570";
+    icon = "steam_icon_2357570";
+    terminal = false;
+    type = "Application";
+    categories = [ "Game" ];
+    settings.StartupWMClass = "steam_app_2357570";
+  };
 }
