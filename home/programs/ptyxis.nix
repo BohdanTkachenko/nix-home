7d2ec4d0850f15c619500a86d185923f6f200265
Bohdan Tkachenko <bohdant@google.com>
2025-12-19 15:08:00 -0500

    feat(ptyxis): add work profile for work laptop that connects to the workstation

--- home/programs/ptyxis.nix
+++ home/programs/ptyxis.nix
@@ -2,11 +2,26 @@
   config,
   lib,
   pkgs,
+  isWorkLaptop,
   ...
 }:
 
 let
-  profileUuid = "C0FFEE-C0DE-FEED-FACE-AC1DDEADBEEF";
+  defaultProfileUuid = "C0FFEE-C0DE-FEED-FACE-AC1DDEADBEEF";
+  defaultProfile = {
+    label = "The Coffee Coder";
+    opacity = lib.gvariant.mkDouble 0.9;
+    palette = "Japanesque";
+    cell-height-scale = 1.0;
+  };
+
+  workWorkstationUuid = "60061E-CAFE-F00D-FA57-0FF1CEACCE55";
+  workWorkstationProfile = defaultProfile // {
+    label = "The Free Food Eater";
+    palette = "VS Code";
+    use-custom-command = true;
+    custom-command = "ssh ws";
+  };
 in
 
 {
@@ -28,16 +43,19 @@ in
     };
 
     "org/gnome/Ptyxis" = {
-      default-profile-uuid = profileUuid;
-      profile-uuids = [ profileUuid ];
+      default-profile-uuid = defaultProfileUuid;
+      profile-uuids = [ defaultProfileUuid ] ++ (if isWorkLaptop then [ workWorkstationUuid ] else [ ]);
       use-system-font = true;
     };
 
-    "org/gnome/Ptyxis/Profiles/${profileUuid}" = {
-      label = "The Coffee Coder";
-      opacity = lib.gvariant.mkDouble 0.9;
-      palette = "Japanesque";
-      cell-height-scale = 1.0;
-    };
-  };
+    "org/gnome/Ptyxis/Profiles/${defaultProfileUuid}" = defaultProfile;
+  }
+  // (
+    if isWorkLaptop then
+      {
+        "org/gnome/Ptyxis/Profiles/${workWorkstationUuid}" = workWorkstationProfile;
+      }
+    else
+      { }
+  );
 }
