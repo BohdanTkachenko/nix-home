08441b817dcb7f8298a1bf551de299181a4cb6c5
Bohdan Tkachenko <bohdant@google.com>
2025-12-22 19:27:26 -0500

    feat(hardware): configure xremap for X1 Carbon Gen 12
    Adds key remappings for the Lenovo ThinkPad X1 Carbon Gen 12 to provide more intuitive navigation and media controls.
    
    - Maps 'Home', 'End', and 'Insert' keys to 'PreviousSong', 'PlayPause', and 'NextSong'.
    - Reassigns 'PageUp' and 'PageDown' to function as 'Home' and 'End'.
    - Implements 'Ctrl+Up/Down' for 'PageUp/PageDown' scrolling.
    - Implements 'Ctrl+Left/Right' for workspace/tab navigation.

--- home/hardware/lenovo-thinkpad-x1-carbon-gen12.nix
+++ home/hardware/lenovo-thinkpad-x1-carbon-gen12.nix
@@ -3,11 +3,32 @@
   services.xremap.config.modmap = [
     {
       name = "Remap special function keys to media keys";
-      device.only = [ "ThinkPad Extra Buttons" ];
+      device.only = [ "AT Translated Set 2 keyboard" ];
       remap = {
-        # "KEY_447" = "KEY_PLAYPAUSE";
-        "KEY_SWITCHVIDEOMODE" = "KEY_PLAYPAUSE";
-        "KEY_SELECTIVE_SCREENSHOT" = "KEY_PREVIOUSSONG";
+        home = "key_previoussong";
+        end = "key_playpause";
+        insert = "key_nextsong";
+      };
+    }
+    {
+      name = "Remap PageUp and PageDown keys to Home/End";
+      device.only = [ "AT Translated Set 2 keyboard" ];
+      remap = {
+        pageup = "home";
+        pagedown = "end";
+      };
+    }
+  ];
+
+  services.xremap.config.keymap = [
+    {
+      name = "Remap navigation keys";
+      device.only = [ "AT Translated Set 2 keyboard" ];
+      remap = {
+        "C-up" = "pageup";
+        "C-down" = "pagedown";
+        "C-left" = "C-pageup";
+        "C-right" = "C-pagedown";
       };
     }
   ];
