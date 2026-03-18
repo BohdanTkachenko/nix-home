19b7a215f05cfbca05d0c4dc6cd9ae03f4e17181
Bohdan Tkachenko <bohdan@tkachenko.dev>
2026-03-17 20:18:56 -0400

    feat(nixos): add GStreamer multimedia plugins to desktop packages

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -95,8 +95,19 @@
     ]
   );
 
-  environment.systemPackages = with pkgs; [
-    google-chrome
-    protontricks
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      google-chrome
+      protontricks
+    ]
+    ++ (with gst_all_1; [
+      gstreamer
+      gst-plugins-base
+      gst-plugins-good
+      gst-plugins-bad
+      gst-plugins-ugly
+      gst-libav
+      gst-vaapi
+    ]);
 }
