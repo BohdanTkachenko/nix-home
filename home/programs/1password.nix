6a5b822be8e5bc20dd2accd18addea7ee5e50496
Bohdan Tkachenko <bohdan@tkachenko.dev>
2026-04-05 01:54:40 -0400

    refactor(1password): remove unused SSH agent override block

--- home/programs/1password.nix
+++ home/programs/1password.nix
@@ -6,9 +6,4 @@
 }:
 {
   home.packages = [ (config.lib.nixGL.wrap pkgs._1password-gui) ];
-
-  programs.ssh.extraConfig = lib.mkIf (!config.my.google.enable) ''
-    Host *
-      IdentityAgent ~/.1password/agent.sock
-  '';
 }
