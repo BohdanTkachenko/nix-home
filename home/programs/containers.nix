7bbc335f2f3c1ae51063774451bccba0ab147152
Bohdan Tkachenko <bohdan@tkachenko.dev>
2026-05-23 21:45:57 -0400

    fix(containers): configure ignore_chown_errors in user storage.conf to allow rootless pulls in sandbox

--- home/programs/containers.nix
+++ home/programs/containers.nix
@@ -3,4 +3,11 @@
   xdg.configFile."containers/policy.json".text = builtins.toJSON {
     default = [ { type = "insecureAcceptAnything"; } ];
   };
+
+  xdg.configFile."containers/storage.conf".text = ''
+    [storage]
+    driver = "overlay"
+    [storage.options]
+    ignore_chown_errors = "true"
+  '';
 }
